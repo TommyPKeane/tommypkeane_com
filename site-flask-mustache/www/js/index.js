6afe83d28f51a9eaf95abf2b5c10061e44da3616
Tommy P. Keane <talk@tommypkeane.com>
2020-07-07 02:10:24 -0400

    UPD: Scroll-to-Top Adjustment
    - Requiring 300px of scrolling before the button shows
    - This should help prevent having the button overlay on the timeline on
    the CV page.

--- site-flask-mustache/www/js/index.js
+++ site-flask-mustache/www/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(
    function hst_ready_func() {
       $(window).scroll(
          function hst_window_scroll_func() {
-            if ($(this).scrollTop() > 50) {
+            if ($(this).scrollTop() > 300) {
                 $("#back-to-top").fadeIn();
             } else {
                 $("#back-to-top").fadeOut();
