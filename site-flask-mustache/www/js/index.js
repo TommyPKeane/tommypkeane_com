cb0a4f2d0f4a390cb6d0ee85de5924a1f7a6d3a7
Tommy P. Keane <talk@tommypkeane.com>
2020-07-12 12:59:15 -0400

    UPD: Moving JS Code for Common Use
    - Moving the generic functions for collapsing a page-intro paragraph to
    the common `index.js` code file.

--- site-flask-mustache/www/js/index.js
+++ site-flask-mustache/www/js/index.js
@@ -1,3 +1,56 @@
+$("#expand-intro").toggle("display");
+$("#collapse-intro").fadeIn();
+
+$("#collapse-intro").mouseover(
+   function () {
+      $("#collapse-intro").tooltip("show");
+      return;
+   }
+);
+
+$("#collapse-intro").mouseout(
+   function () {
+      $("#collapse-intro").tooltip("hide");
+      return;
+   }
+);
+
+$("#collapse-intro").click(
+   function () {
+      $("#collapse-intro").tooltip("hide");
+      $("#expand-intro").tooltip("hide");
+      $("#collapse-intro").toggle("display");
+      $("#expand-intro").toggle("display");
+      $(".intro_par").toggle("display");
+      return false;
+   }
+);
+
+$("#expand-intro").mouseover(
+   function () {
+      $("#expand-intro").tooltip("show");
+      return;
+   }
+);
+
+$("#expand-intro").mouseout(
+   function () {
+      $("#expand-intro").tooltip("hide");
+      return;
+   }
+);
+
+$("#expand-intro").click(
+   function () {
+      $("#collapse-intro").tooltip("hide");
+      $("#expand-intro").tooltip("hide");
+      $("#collapse-intro").toggle("display");
+      $("#expand-intro").toggle("display");
+      $(".intro_par").toggle("display");
+      return false;
+   }
+);
+
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
