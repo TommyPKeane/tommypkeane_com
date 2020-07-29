9a2452678c2adeabe548775b725b75a76ffd68b7
Tommy P. Keane <talk@tommypkeane.com>
2020-07-28 23:25:02 -0400

    UPD: Icon References Updated and Moved Scope
    - Moved dictionary to outer scope to prevent image files from being
    re-read on every request (not sure if that will actually work).
    - Added new TommyTofu icons to the icon set to replace more feather
    icons.

--- site-flask-mustache/www/src/http_request.py
+++ site-flask-mustache/www/src/http_request.py
@@ -8,41 +8,41 @@ import yaml;
 from . import _base;
 from . import _util;
 
+icon_data_dct = {
+   # FEATHER ICONS
+   "icon_bitbucket": _util.get_file_contents_str("./img/feather/droplet.svg"),
+   "icon_google_scholar": _util.get_file_contents_str("./img/feather/award.svg"),
+   "icon_instagram": _util.get_file_contents_str("./img/feather/instagram.svg"),
+   "icon_facebook": _util.get_file_contents_str("./img/feather/facebook.svg"),
+   "icon_twitter": _util.get_file_contents_str("./img/feather/twitter.svg"),
+   "icon_linkedin": _util.get_file_contents_str("./img/feather/linkedin.svg"),
+   "icon_raspberrypi": _util.get_file_contents_str("./img/feather/cpu.svg"),
+   "icon_pinetime": _util.get_file_contents_str("./img/feather/watch.svg"),
+   "icon_pinephone": _util.get_file_contents_str("./img/feather/phone.svg"),
+   "icon_contact": _util.get_file_contents_str("./img/feather/phone.svg"),
+   "exif_icon": _util.get_file_contents_str("./img/feather/film.svg"),
+   "fullsize_icon": _util.get_file_contents_str("./img/feather/external-link.svg"),
+   # TOMMYTOFU ICONS
+   "icon_home": _util.get_file_contents_str("./img/tommytofu/home-cabin-a.svg"),
+   "icon_cv_resume": _util.get_file_contents_str("./img/tommytofu/skull-round-a.svg"),
+   "icon_books": _util.get_file_contents_str("./img/tommytofu/book-closed.svg"),
+   "icon_settings": _util.get_file_contents_str("./img/tommytofu/config.svg"),
+   "icon_other_sites": _util.get_file_contents_str("./img/tommytofu/menu.svg"),
+   "icon_github": _util.get_file_contents_str("./img/tommytofu/git-octocat.svg"),
+   "icon_photography": _util.get_file_contents_str("./img/tommytofu/camera-photo.svg"),
+   "img_scroll_to_top": _util.get_file_contents_str("./img/tommytofu/bracket-angle-d-up.svg"),
+   "img_intro_collapse": _util.get_file_contents_str("./img/tommytofu/bracket-angle-d-up.svg"),
+   "img_intro_expand": _util.get_file_contents_str("./img/tommytofu/bracket-angle-d-dn.svg"),
+   # OTHERS
+   "ico_png_file": "/img/tommypkeane-com_ico_circle-t.png",
+   "ico_ico_file": "/img/tommypkeane-com_ico_circle-t.ico",
+};
+
 
 def _get_template_data_icons():
    """Get the template replacement data for the common site navbar.
    """
-   data_dct = {
-      # FEATHER ICONS
-      "icon_home": _util.get_file_contents_str("./img/feather/home.svg"),
-      "icon_cv_resume": _util.get_file_contents_str("./img/feather/briefcase.svg"),
-      "icon_bitbucket": _util.get_file_contents_str("./img/feather/droplet.svg"),
-      "icon_google_scholar": _util.get_file_contents_str("./img/feather/award.svg"),
-      "icon_instagram": _util.get_file_contents_str("./img/feather/instagram.svg"),
-      "icon_facebook": _util.get_file_contents_str("./img/feather/facebook.svg"),
-      "icon_twitter": _util.get_file_contents_str("./img/feather/twitter.svg"),
-      "icon_linkedin": _util.get_file_contents_str("./img/feather/linkedin.svg"),
-      "icon_raspberrypi": _util.get_file_contents_str("./img/feather/cpu.svg"),
-      "icon_pinetime": _util.get_file_contents_str("./img/feather/watch.svg"),
-      "icon_pinephone": _util.get_file_contents_str("./img/feather/phone.svg"),
-      "icon_publications": _util.get_file_contents_str("./img/feather/file-text.svg"),
-      "img_scroll_to_top": _util.get_file_contents_str("./img/feather/chevrons-up.svg"),
-      "img_intro_collapse": _util.get_file_contents_str("./img/feather/chevrons-up.svg"),
-      "img_intro_expand": _util.get_file_contents_str("./img/feather/chevrons-down.svg"),
-      "icon_contact": _util.get_file_contents_str("./img/feather/phone.svg"),
-      "exif_icon": _util.get_file_contents_str("./img/feather/film.svg"),
-      "fullsize_icon": _util.get_file_contents_str("./img/feather/external-link.svg"),
-      # TOMMYTOFU ICONS
-      "icon_books": _util.get_file_contents_str("./img/tommytofu/book-closed.svg"),
-      "icon_settings": _util.get_file_contents_str("./img/tommytofu/config.svg"),
-      "icon_other_sites": _util.get_file_contents_str("./img/tommytofu/menu.svg"),
-      "icon_github": _util.get_file_contents_str("./img/tommytofu/git-octocat.svg"),
-      "icon_photography": _util.get_file_contents_str("./img/tommytofu/camera-photo.svg"),
-      # OTHERS
-      "ico_png_file": "/img/tommypkeane-com_ico_circle-t.png",
-      "ico_ico_file": "/img/tommypkeane-com_ico_circle-t.ico",
-   };
-   return (data_dct);
+   return (icon_data_dct);
 # fed
 
 
